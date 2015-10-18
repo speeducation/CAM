6baff7f67ff700616ffb741b094a2640117aaa30
Daniel Rivera Guzman <drivera1@uabc.edu.mx>
2015-10-18 13:10:33 -0700

    Se modifico el archivo settings.py donde se indica, la configuracion base y la configuracion local de trabajo

--- Speeducation/Speeducation/settings/base.py
+++ Speeducation/Speeducation/settings/base.py
@@ -1,6 +1,12 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from unipath import Path
-BASE_DIR = Path(__file__).ancestor(2)
+BASE_DIR = Path(__file__).ancestor(3)
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8ce%wc$91p%8mx)%rd=bgo68^5b9eny0qf^f5rbslbiz@*9h*2'
 
 DJANGO_APPS = (
@@ -24,7 +30,6 @@ LOCAL_APPS = (
 
 INSTALLED_APPS = DJANGO_APPS + THIRDPARTY_APPS + LOCAL_APPS
 
-
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,6 +42,9 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'Speeducation.urls'
 
 WSGI_APPLICATION = 'Speeducation.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
