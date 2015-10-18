4ddeb0c090161fa91e464af11896bbe8e9d5c690
Daniel Rivera Guzman <drivera1@uabc.edu.mx>
2015-10-18 15:19:51 -0700

    Se creo un app base, que tendra una configuracion como base, para que manejen las demas apps del proyecto, se anadieron comentarios de ayuda para comprender mejor las funciones del proyecto

--- Speeducation/apps/base/urls.py
+++ Speeducation/apps/base/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import IndexView
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view()),
+    url(r'^$', IndexView.as_view()),    #Cuando la aplicacion solicite la peticion https://127.0.0.1:8000/ o http://localhost:8000/
+                                        #Desplegara la vista contenida en la clase IndexView el cual es base/index.html
 )
